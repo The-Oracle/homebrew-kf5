025d2bdf95db983ad95179bf4d39366eaf13fe5a
Jeremy Brown <Jeremy@jbdynamics.net>
2017-04-23 23:27:08 -0600

    Fix attempt #1 for url and head

--- kf5-kdeui.rb
+++ kf5-kdeui.rb
@@ -1,11 +1,11 @@
 require "formula"
 
 class Kf5Kdeui < Formula
-  url "file:///Users/diradmin/Downloads/kdelibs-master/kdeui-5.32.0.tar.xz"
+  url "https://github.com/The-Oracle/homebrew-kf5/raw/5abff398466d1a4519dc62683ac4272198c7a274/kdeui-5.32.0.tar.xz"
   sha256 "59009aaa5b23aea3a49d79652fffaff6f549d433f19812808f2b1de81f33143a"
   homepage "http://www.kde.org/"
 
-  head 'file://Users/diradmin/Downloads/kdelibs-master/kdeui/.git'
+  head 'git://github.com/The-Oracle/kdeui.git'
 
   depends_on "cmake" => :build
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
